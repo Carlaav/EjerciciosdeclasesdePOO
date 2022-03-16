32908c7da4cc35fec3e8fbb41745df1325a3d56e
Carlaav <carla2002.av@gmail.com>
2022-03-16 17:59:20 +0100

    a

--- c.py
+++ c.py
@@ -1,1 +1,7 @@
+#el primer print da la clase A
+#el segundo print devolvera true porque se cumple
+#el tercer print devuelve 0 porque le pasas una tupla
+#el cuarto devuelve 1 porque llama a la funcion y de la clase a y le pasa la tupla con un elemento
+#el quinto devuelve 2 porque llama a la funcion y de A donde en el self le pasa aa que es la clase a y luego una tupla de dos elementos
+#el sexto devuelve 3 porque vuelve a hacer lo anterior pasando 3 elementos
 
